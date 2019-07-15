cba9c51f422d6f5a3576c045617c55479594387e
markuslee <ben0133@163.com>
2019-07-15 17:31:21 +0800

    Add number of files by progressbar

--- TaoAlbum/app/src/main/java/cn/cloudfk/taoalbum/activity/UploadActivity.java
+++ TaoAlbum/app/src/main/java/cn/cloudfk/taoalbum/activity/UploadActivity.java
@@ -3,14 +3,19 @@ package cn.cloudfk.taoalbum.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import cn.cloudfk.taoalbum.R;
 import cn.cloudfk.taoalbum.service.UploadService;
 import cn.cloudfk.taoalbum.view.UploadProgressBar;
 
 public class UploadActivity extends AppCompatActivity {
-    UploadProgressBar progressBar;
-    int i = 0;
+    private UploadProgressBar progressBar;
+    private Toolbar mToolbar;
+    private TextView uploadSizeTxt;
+    String uploadSizeStr = "0/0";
     int progressBarMax = 0;
 
     private static final String TAG = "UploadActivity";
@@ -19,10 +24,14 @@ public class UploadActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.upload_progress);
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
         Intent startIntent = new Intent(this, UploadService.class);
         startService(startIntent);
         progressBar = findViewById(R.id.uploadProgressBar);
         progressBarMax = progressBar.getMax();
+        uploadSizeTxt = findViewById(R.id.uploadSizeTxt);
+
 
         new Thread(new Runnable() {
             @Override
@@ -32,11 +41,15 @@ public class UploadActivity extends AppCompatActivity {
                     if(UploadService.UploadFiles >0
                             &&(UploadService.UploadCurrent==UploadService.UploadFiles)){
                         progressBar.setTargetProgress(progressBarMax);
+                        uploadSizeStr = UploadService.UploadFiles+"/"+UploadService.UploadFiles;
+
                     }else if(UploadService.UploadFiles>0) {
-                        float valueF =((float)UploadService.UploadCurrent/(float)UploadService.UploadFiles);
+                        float valueF =(float)UploadService.UploadCurrent/(float)UploadService.UploadFiles;
                         //Log.i(TAG,"UploadComplete="+ valueF+"%");
+                        uploadSizeStr = UploadService.UploadCurrent+"/"+UploadService.UploadFiles;
                         progressBar.setTargetProgress((int)(valueF*100));
                     }
+                    uploadSizeTxt.setText(uploadSizeStr);
                     try {
                         Thread.sleep(500);
                     } catch (Exception e) {
@@ -46,5 +59,18 @@ public class UploadActivity extends AppCompatActivity {
             }
         }).start();
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home: {
+                finish();
+                break;
+            }
+        }
+        return true;
     }
 }
